6e57a0204ffee89a528a429b2ca7a67e716e5f36
Harshith <harshith@gmail.com>
2026-02-16 17:44:13 +0530

    Update project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./components/dashboard";
+import Dashboard from "./components/Dashboard";
 import OverallAttendance from "./pages/OverallAttendance";
 import ManageSubjects from "./pages/ManageSubjects";
 import AllSubjects from "./pages/AllSubjects";
 import Settings from "./pages/Settings";
 import Suggestion from "./pages/Suggestion";
 import ReportBug from "./pages/ReportBug";
+
 const App = () => {
   return (
     <BrowserRouter>
@@ -14,10 +15,10 @@ const App = () => {
         <Route path="/" element={<Dashboard />} />
         <Route path="/overall" element={<OverallAttendance />} />
         <Route path="/Manage" element={<ManageSubjects />} />
-       <Route path="/Allsubjects" element={<AllSubjects/>}/>
-       <Route path="/Setting" element={<Settings/>}/>
-       <Route path="/Suggestion" element={<Suggestion/>}/>
-       <Route path="/Report" element={<ReportBug/>}/>
+        <Route path="/Allsubjects" element={<AllSubjects />} />
+        <Route path="/Setting" element={<Settings />} />
+        <Route path="/Suggestion" element={<Suggestion />} />
+        <Route path="/Report" element={<ReportBug />} />
       </Routes>
     </BrowserRouter>
   );
